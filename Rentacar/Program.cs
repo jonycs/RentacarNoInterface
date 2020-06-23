8166fc2745904b40657f358fe8ca2ae9256e5329
Jonatas <jonatascstambouli@gmail.com>
2020-06-23 13:29:49 -0300

    Montagem dos serviços e tela

--- Rentacar/Program.cs
+++ Rentacar/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Rentacar.Entities;
+using Rentacar.Services;
+using System;
+using System.Globalization;
 
 namespace Rentacar
 {
@@ -6,7 +9,33 @@ namespace Rentacar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Entre com os dado do aluguel:");
+            Console.Write("Modelo do carro: ");
+            string modelo = Console.ReadLine();
+            Console.Write("Retirada (DD/MM/AAAA HH:MM): ");
+            DateTime retirada = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Entrega (DD/MM/AAAA HH:MM): ");
+            DateTime entrega = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Preço por hora: R$");
+            double precoHora = double.Parse(Console.ReadLine());
+            Console.Write("Preço por dia: R$");
+            double precoDia = double.Parse(Console.ReadLine());
+
+            CarRental cr = new CarRental(retirada, entrega, new Vehicle(modelo));
+
+            RentalService rs = new RentalService(precoHora, precoDia);
+
+            rs.ProcessInvoice(cr);
+
+            Console.WriteLine("INVOICE:");
+
+            Console.WriteLine(cr.Invoice);
+
+
+            
+
+            
         }
     }
 }
